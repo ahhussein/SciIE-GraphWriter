975d842e4745ca619f798f4b498e70d7c383808d
Ahmed <ahmed@JieyideMacBook-Pro.local>
2020-09-03 15:37:35 +0200

    Update cpp timing

--- span_prune.cpp
+++ span_prune.cpp
@@ -23,7 +23,6 @@ torch::Tensor extract_spans(
     freopen( "cpp.log", "a", stdout );
       //freopen( "error.txt", "w", stderr );
 
-   time_t givemetime = time(NULL);
 
     int num_sentences = span_scores.size(0);
     int num_input_spans = span_scores.size(1);
@@ -36,6 +35,7 @@ torch::Tensor extract_spans(
         max_num_output_spans = num_output_spans[i].item<int64_t>();
       }
     }
+   time_t givemetime = time(NULL);
 
       cout << ctime(&givemetime) <<" max num outputs: " << max_num_output_spans << endl;
 
@@ -45,6 +45,8 @@ torch::Tensor extract_spans(
                                                             std::vector<int>(num_input_spans));
 
     torch::Tensor output_span_indices = torch::ones({num_sentences, max_num_output_spans});
+
+   time_t givemetime = time(NULL);
 
       cout << ctime(&givemetime) <<" Sorting span indices: Started" << endl;
 
@@ -59,6 +61,8 @@ torch::Tensor extract_spans(
                   return span_scores[i][j2].item<int64_t>() < span_scores[i][j1].item<int64_t>();
                 });
     }
+       time_t givemetime = time(NULL);
+
       cout << ctime(&givemetime) <<" Sorting span indices: Completed" << endl;
 
 
@@ -70,12 +74,15 @@ torch::Tensor extract_spans(
 
 
     for (int l = 0; l < num_sentences; l++) {
+       time_t givemetime = time(NULL);
+
       cout << ctime(&givemetime) <<" Inside faulty loop - sentence " << (l+1) << " out of: "<< num_sentences << endl;
 
       std::vector<int> top_span_indices;
       std::unordered_map<int, int> end_to_earliest_start;
       std::unordered_map<int, int> start_to_latest_end;
       int current_span_index = 0, num_selected_spans = 0;
+   time_t givemetime = time(NULL);
 
       cout << ctime(&givemetime) <<" Fauly Loop - corssing while - Started" << endl;
 
@@ -128,11 +135,14 @@ torch::Tensor extract_spans(
         }
         ++current_span_index;
       }
+   time_t givemetime = time(NULL);
 
       cout << ctime(&givemetime) <<" Fauly Loop - corssing while - Completed" << endl;
 
       // Sort and produce span indices.
       if (_sort_spans) {
+         time_t givemetime = time(NULL);
+
         cout << ctime(&givemetime) <<" Fauly Loop - Sort Spans - Started" << endl;
 
         std::sort(top_span_indices.begin(), top_span_indices.end(),
@@ -153,6 +163,7 @@ torch::Tensor extract_spans(
                     return i1 < i2;
                   }
                 });
+   time_t givemetime = time(NULL);
 
         cout << ctime(&givemetime) <<" Fauly Loop - Sort Spans - Completed" << endl;
         cout << ctime(&givemetime) <<" num out spans size " << num_output_spans[l].item<int64_t>()<< endl;
@@ -170,11 +181,14 @@ torch::Tensor extract_spans(
           output_span_indices[l][i]= output_span_indices[l][last_selected].item<int64_t>();
         }
       }
+         time_t givemetime = time(NULL);
+
       cout << ctime(&givemetime) <<" Inside faulty loop - sentence " << (l+1) << " Finished" << endl;
 
 
     }
 
+   time_t givemetime = time(NULL);
   cout <<" =======" << endl;
 
 
