d5712c1bd7e0fb500b606ee2b438456ad3bb65cf
Ahmed Hussein <a.abdelaziz91@gmail.com>
2020-09-22 12:31:35 +0200

    fix lgoger

--- generator.py
+++ generator.py
@@ -86,7 +86,7 @@ def test(ds, graph_model, model_name, epoch='cmdline'):
   logger.info(f"Bleu_3:\t {final_scores['Bleu_3']}")
   logger.info(f"Bleu_4:\t {final_scores['Bleu_4']}")
   logger.info(f"ROUGE_L:\t {final_scores['ROUGE_L']}")
-  logger.info('METEOR:\t', final_scores['METEOR'])
+  logger.info(f"METEOR:\t {final_scores['METEOR']}")
   #logger.info('CIDEr:\t', final_scores['CIDEr'])
 
   return preds,golds
